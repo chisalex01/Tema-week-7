659f49cea0b62ae86910ad179c530cec1de00c21
chisalex01 <chisalex01@yahoo.com>
2021-06-10 23:54:00 +0300

    Add project files.

---
+++ Tema week 7/Data/Tema_week_7Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tema_week_7.Models;
+
+namespace Tema_week_7.Data
+{
+    public class Tema_week_7Context : DbContext
+    {
+        public Tema_week_7Context (DbContextOptions<Tema_week_7Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tema_week_7.Models.Duckbill> Duckbill { get; set; }
+    }
+}
---
+++ Tema week 7/Models/Duckbill.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Tema_week_7.Models
+{
+    public class Duckbill
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
